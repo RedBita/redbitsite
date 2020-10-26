bfefeaad0fa04fb9e06afb4cdd1f7c5064a0346a
Sebo_the_tramp <sebastian.cavada.lab@gmail.com>
2020-10-26 20:57:28 +0100

    1603742248

--- redbitsite/src/components/Main.vue
+++ redbitsite/src/components/Main.vue
@@ -1,16 +1,16 @@
 <template>
+  <div class="ma-0 pa-0">
     <v-row class="text-center d-flex justify-center ma-0">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/redbit.png')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
 
       <v-col class="mb-4">
         <h1 class="display-2 font-weight-bold mb-3">Welcome to RedBit</h1>
+
+         <v-img
+          src="https://www.hackthebox.eu/badge/team/image/3141"
+          class="my-3"
+          contain
+          max-height="50"
+        />
 
         <p class="subheading font-weight-regular">
           If you wanna hang out with us
@@ -21,15 +21,37 @@
         </p>
       </v-col>
     </v-row>
+    <v-row class="text-center d-flex justify-center ma-0">
+      <h1>If you enjoyed the content here it is our gorgeous team</h1>
+    </v-row>
+
+    <v-row
+      v-for="index in team_mates"
+      :key="index"
+      class="text-center d-flex justify-center ma-0 pb-2"
+    >
+      <v-img
+        max-width="220px"
+        :src="getImageHtbMate(index)"
+      ></v-img>
+    </v-row>
+  </div>
 </template>
 
-
 <script>
-
 export default {
   name: "HelloWorld",
 
-  data: () => ({}),
+  data: () => ({
+    team_mates: ["371504", "265308", "121956", "61590", "26216", "100836", "240627"],
+    //hallo: "<script src='https://www.hackthebox.eu/badge/team/3141'></scri" + "pt>"
+  }),
+  
+  methods: {
+    getImageHtbMate(mate) {
+      return "http://www.hackthebox.eu/badge/image/" + mate
+    },
+  }
 
 };
 </script>
